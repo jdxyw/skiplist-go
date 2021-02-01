662b9d69fab069a79f4233a25c6b23040d9da847
Yongwei Xing <yongwei.xing@liulishuo.com>
2021-02-01 20:03:01 +0800

    add int32 and int64 comparator

--- comparator/comparator.go
+++ comparator/comparator.go
@@ -3,7 +3,7 @@ package comparator
 import "bytes"
 
 type Comparator interface {
-	Compare(rhs, lhs []byte) int
+	Compare(rhs, lhs interface{}) int
 	Name() string
 }
 
@@ -11,10 +11,50 @@ var DefaultComparator Comparator = defaultCmp{}
 
 type defaultCmp struct{}
 
-func (defaultCmp) Compare(rhs, lhs []byte) int {
-	return bytes.Compare(rhs, lhs)
+func (defaultCmp) Compare(rhs, lhs interface{}) int {
+	return bytes.Compare(rhs.([]byte), lhs.([]byte))
 }
 
 func (defaultCmp) Name() string {
 	return "BytewiseComparator"
+}
+
+type Int32Cmp struct {}
+
+func (Int32Cmp) Compare(rhs, lhs interface{}) int {
+	rhsInt := rhs.(int32)
+	lhsInt := lhs.(int32)
+
+	switch res := rhsInt-lhsInt; {
+	case res == 0:
+		return 0
+	case res > 0:
+		return 1
+	default:
+		return -1
+	}
+}
+
+func (Int32Cmp) Name() string{
+	return "Int32Comparator"
+}
+
+type Int64Cmp struct {}
+
+func (Int64Cmp) Compare(rhs, lhs interface{}) int {
+	rhsInt := rhs.(int64)
+	lhsInt := lhs.(int64)
+
+	switch res := rhsInt-lhsInt; {
+	case res == 0:
+		return 0
+	case res > 0:
+		return 1
+	default:
+		return -1
+	}
+}
+
+func (Int64Cmp) Name() string{
+	return "Int32Comparator"
 }
