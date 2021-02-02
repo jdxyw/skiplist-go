52e2bc3505cc24da5c7435c69c32c1f40eaef44b
Yongwei Xing <yongwei.xing@liulishuo.com>
2021-02-02 19:39:18 +0800

    format the file and add some comments

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # skiplist-go
 ![Go](https://github.com/jdxyw/skiplist-go/workflows/Go/badge.svg?branch=main)
+[![codecov](https://codecov.io/gh/jdxyw/skiplist-go/branch/main/graph/badge.svg?token=BK9VMLZKHI)](https://codecov.io/gh/jdxyw/skiplist-go)
+[![Go Report Card](https://goreportcard.com/badge/github.com/jdxyw/skiplist-go)](https://goreportcard.com/report/github.com/jdxyw/skiplist-go)
 
 Skiplist package in Golang
--- comparator/comparator.go
+++ comparator/comparator.go
@@ -2,21 +2,30 @@ package comparator
 
 import "bytes"
 
+// The comparator interface.
+// rhs > lhs : 1
+// rhs == lhs : 0
+// rhs < lhs : -1
+// In this package, only provides the BytewiseComparator comparator.
+// If the key is int32 or int64, you can define the Int32Comparator by yourself.
 type Comparator interface {
 	Compare(rhs, lhs interface{}) int
 	Name() string
 }
 
+// Return the default comparator which is the BytewiseComparator.
 func GetDefaultComparator() Comparator {
 	return BytewiseComparator{}
 }
 
 type BytewiseComparator struct{}
 
-func (_ BytewiseComparator) Compare(rhs, lhs interface{}) int {
+// Compare compare two byte slices for BytewiseComparator.
+func (BytewiseComparator) Compare(rhs, lhs interface{}) int {
 	return bytes.Compare(rhs.([]byte), lhs.([]byte))
 }
 
-func (_ BytewiseComparator) Name() string {
+// Name returns the name of current comparator.
+func (BytewiseComparator) Name() string {
 	return "BytewiseComparator"
 }
